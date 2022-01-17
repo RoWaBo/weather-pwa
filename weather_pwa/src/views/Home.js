05f149ddb29980476261aa59982b8ce4170e02d6
Robert <robertwattboolsen@gmail.com>
2022-01-17 09:42:01 +0100

    Getting position, passing it to openweathermap, recieving response

--- weather_pwa/src/views/Home.js
+++ weather_pwa/src/views/Home.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Home = () => {
+  const [weatherData, setWeatherData] = useState();
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+        axios(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        ).then((response) => setWeatherData(response.data));
+      });
+    }
+  }, []);
+
   return (
     <>
       <h1>this is home</h1>
