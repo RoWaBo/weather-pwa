255712f03544fb635b319d904a113242e542d84f
Robert <robertwattboolsen@gmail.com>
2022-01-17 10:35:17 +0100

    Created handler if location is blocked by user

--- weather_pwa/src/views/Home.js
+++ weather_pwa/src/views/Home.js
@@ -2,19 +2,35 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
+  "geolocation" in navigator
+    ? console.log("available")
+    : console.log("unavailable");
+
   const [weatherData, setWeatherData] = useState();
+  const [locationNotAllowed, setLocationNotAllowed] = useState();
 
   useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
-        axios(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
-        ).then((response) => setWeatherData(response.data));
+    (async () => {
+      // Check if location is allowed
+      const { state: permission } = await navigator.permissions.query({
+        name: "geolocation",
       });
-    }
+      if (permission === "denied") return setLocationNotAllowed(true);
+
+      // Fetch current position OpenWeatherMap data
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+
+          const { data } = await axios(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+          );
+          setWeatherData(data);
+        });
+      }
+    })();
   }, []);
 
   return (
