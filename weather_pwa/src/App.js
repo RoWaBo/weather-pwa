727ad530111357747009fab8a246225dea4a6728
Robert <robertwattboolsen@gmail.com>
2022-01-18 13:58:17 +0100

    Added React-router to index.js and app.js + routes to all views

--- weather_pwa/src/App.js
+++ weather_pwa/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Home from "./views/Home";
+import { Routes, Route } from "react-router-dom";
+import Search from "./views/Search";
+import Favorites from "./views/Favorites";
+import Navigation from "./components/Navigation";
+import MainLayout from "./components/MainLayout";
 
 function App() {
   return (
     <>
-      <Home />
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="favorites" element={<Favorites />} />
+        </Routes>
+      </MainLayout>
+      <Navigation />
     </>
   );
 }
