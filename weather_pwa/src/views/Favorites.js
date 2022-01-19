e09469e69a22a6ae0c8b14d4d5adf9a8c7ddef8e
Robert <robertwattboolsen@gmail.com>
2022-01-19 15:11:05 +0100

    Looping over favorites and recieving data from API in Favorites

--- weather_pwa/src/views/Favorites.js
+++ weather_pwa/src/views/Favorites.js
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Favorites = () => {
+  const favoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    if (favoriteCities) {
+      let cityWeather = [];
+      favoriteCities.forEach(async (cityName) => {
+        try {
+          const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+
+          const { data } = await axios(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+          );
+
+          cityWeather.push(data);
+        } catch (err) {
+          console.log(err);
+        }
+      });
+      setWeather(cityWeather);
+    }
+  }, []);
+
   return <h1>this is the favorites view</h1>;
 };
 
