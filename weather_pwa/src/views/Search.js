57026adcfe11dd4e2ddd190c31ad1bba3ef2f36f
Robert <robertwattboolsen@gmail.com>
2022-01-19 10:12:55 +0100

    Created form error message

--- weather_pwa/src/views/Search.js
+++ weather_pwa/src/views/Search.js
@@ -2,15 +2,31 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import axios from "axios";
 
 const Search = () => {
   const { cityName } = useParams();
 
   const [searchInput, setSearchInput] = useState("");
+  const [formError, setFormError] = useState(false);
+  const [cityWeather, setCityWeather] = useState("");
 
-  const fetchCityWeather = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(searchInput);
+    searchInput !== "" ? fetchCityWeather() : setFormError(true);
+  };
+  const fetchCityWeather = async () => {
+    try {
+      const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+      const city = cityName === "searching" ? searchInput : cityName;
+
+      const { data } = await axios(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
+      setCityWeather(data);
+    } catch (err) {
+      alert(err);
+    }
   };
 
   // === STYLE ===
@@ -62,10 +78,15 @@ const Search = () => {
     box-shadow: rgba(50, 50, 93, 0.1) 0px 6px 12px -2px,
       rgba(0, 0, 0, 0.2) 0px 3px 7px -3px;
   `;
+  const formErrorStyle = css`
+    font-size: 15px;
+    color: #ff2727;
+    margin: 1.5rem 0 0;
+  `;
   if (cityName === "searching")
     return (
       <div css={containerStyle}>
-        <form onSubmit={fetchCityWeather} css={formStyle}>
+        <form onSubmit={handleSubmit} css={formStyle}>
           <label name="search" id="search" css={labelStyle}>
             Enter a city name
           </label>
@@ -76,7 +97,9 @@ const Search = () => {
             value={searchInput}
             onChange={(e) => setSearchInput(e.target.value)}
             css={inputStyle}
+            onFocus={() => setFormError(false)}
           />
+          {formError && <p css={formErrorStyle}>You must enter a city name!</p>}
           <input type="submit" value="Search" css={submitStyle} />
         </form>
       </div>
