8bd24dc3d97a7ad95103020efc1b8190abb948df
Robert <robertwattboolsen@gmail.com>
2022-01-19 09:45:36 +0100

    Created and styled search form in Search

--- weather_pwa/src/views/Search.js
+++ weather_pwa/src/views/Search.js
@@ -1,11 +1,86 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 const Search = () => {
   const { cityName } = useParams();
 
-  console.log(cityName);
+  const [searchInput, setSearchInput] = useState("");
 
-  return <h1>this is the search view</h1>;
+  const fetchCityWeather = (e) => {
+    e.preventDefault();
+    console.log(searchInput);
+  };
+
+  // === STYLE ===
+  const containerStyle = css`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+  `;
+  const labelStyle = css`
+    display: block;
+    font-size: 20px;
+    font-weight: 500;
+    margin-bottom: 1rem;
+  `;
+  const inputStyle = css`
+    display: block;
+    width: 100%;
+    border: none;
+    border-radius: 10px;
+    outline: none;
+    padding: 0.8rem;
+    font-size: 14px;
+    background: #fffcfd;
+    box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+
+    &:focus {
+      box-shadow: #e0aca3 0px 1px 3px 0px, #e0aca3 0px 0px 0px 1px;
+    }
+  `;
+  const formStyle = css`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
+  const submitStyle = css`
+    padding: 0.5rem 2rem;
+    width: 50%;
+    border: 1px solid rgba(59, 60, 58, 0.1);
+    border-radius: 10px;
+    background: #e0aca3;
+    margin: 2rem 0;
+    font-size: 20px;
+    color: white;
+    box-shadow: rgba(50, 50, 93, 0.1) 0px 6px 12px -2px,
+      rgba(0, 0, 0, 0.2) 0px 3px 7px -3px;
+  `;
+  if (cityName === "searching")
+    return (
+      <div css={containerStyle}>
+        <form onSubmit={fetchCityWeather} css={formStyle}>
+          <label name="search" id="search" css={labelStyle}>
+            Enter a city name
+          </label>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            css={inputStyle}
+          />
+          <input type="submit" value="Search" css={submitStyle} />
+        </form>
+      </div>
+    );
 };
 
 export default Search;
