d5f9a2b0e6164da0048fb781b19c851c8c3af85d
Robert <robertwattboolsen@gmail.com>
2022-01-20 10:03:18 +0100

    Added Loading and no favorites error message

--- weather_pwa/src/views/Favorites.js
+++ weather_pwa/src/views/Favorites.js
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { css } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
+import LoadingSpinner from "../components/LoadingSpinner";
+import CenterContainer from "../components/CenterContainer";
+import { ImHeartBroken } from "react-icons/im";
 
 const Favorites = () => {
   const [weather, setWeather] = useState();
   const navigate = useNavigate();
+  const favoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
 
   useEffect(() => {
     const favoriteCities =
@@ -27,7 +33,38 @@ const Favorites = () => {
     }
   }, [navigate]);
 
-  return <h1>this is the favorites view</h1>;
+  // === STYLE ===
+  const headingStyle = css`
+    font-size: 20px;
+    text-align: center;
+  `;
+  const headerStyle = css`
+    padding: 1rem;
+  `;
+  const errorMessageStyle = css`
+    font-size: 20px;
+  `;
+  const errorIconStyle = css`
+    font-size: 60px;
+    margin-bottom: 2rem;
+    color: #3b3c3a;
+  `;
+
+  return (
+    <>
+      <header css={headerStyle}>
+        <h1 css={headingStyle}>Your favorites</h1>
+      </header>
+      {weather && <ul>{}</ul>}
+      {!weather && favoriteCities && <LoadingSpinner />}
+      {!favoriteCities && (
+        <CenterContainer>
+          <ImHeartBroken css={errorIconStyle} />
+          <h2 css={errorMessageStyle}>You have no favorites</h2>
+        </CenterContainer>
+      )}
+    </>
+  );
 };
 
 export default Favorites;
