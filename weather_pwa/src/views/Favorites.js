52f5e582bfb52bf090bd1095f206818ce7b3f776
Robert <robertwattboolsen@gmail.com>
2022-01-20 08:21:21 +0100

    Fixed infinite useEffect loop in Favorites

--- weather_pwa/src/views/Favorites.js
+++ weather_pwa/src/views/Favorites.js
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Favorites = () => {
-  const favoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
   const [weather, setWeather] = useState();
 
   useEffect(() => {
-    if (favoriteCities) {
+    const favoriteCities =
+      JSON.parse(localStorage.getItem("favoriteCities")) || [];
+
+    if (favoriteCities.length > 0) {
       let cityWeather = [];
       favoriteCities.forEach(async (cityName) => {
         try {
           const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
-
           const { data } = await axios(
             `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
           );
-
           cityWeather.push(data);
         } catch (err) {
           console.log(err);
