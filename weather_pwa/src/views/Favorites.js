8def273f8fd9d775a1291a2a07d5c04b713d22a8
Robert <robertwattboolsen@gmail.com>
2022-01-20 11:46:35 +0100

    Favorite view trying to bug fix

--- weather_pwa/src/views/Favorites.js
+++ weather_pwa/src/views/Favorites.js
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { css } from "@emotion/react";
 /** @jsxImportSource @emotion/react */
 import LoadingSpinner from "../components/LoadingSpinner";
 import CenterContainer from "../components/CenterContainer";
 import { ImHeartBroken } from "react-icons/im";
+import SmallWeatherInfoItem from "../components/SmallWeatherInfoItem";
 
 const Favorites = () => {
   const [weather, setWeather] = useState();
   const navigate = useNavigate();
-  const favoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
+  const [favoriteCities, setFavoriteCities] = useState(
+    JSON.parse(localStorage.getItem("favoriteCities")) || []
+  );
 
   useEffect(() => {
-    const favoriteCities =
-      JSON.parse(localStorage.getItem("favoriteCities")) || [];
-
     if (favoriteCities.length > 0) {
       let cityWeather = [];
+
       favoriteCities.forEach(async (cityName) => {
         try {
           const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
@@ -25,13 +26,15 @@ const Favorites = () => {
             `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
           );
           cityWeather.push(data);
+          // setWeather([...weather, data]);
         } catch (err) {
           navigate("/Fallback");
         }
       });
+
       setWeather(cityWeather);
     }
-  }, [navigate]);
+  }, [navigate, favoriteCities]);
 
   // === STYLE ===
   const headingStyle = css`
@@ -50,19 +53,43 @@ const Favorites = () => {
     color: #3b3c3a;
   `;
 
+  weather && console.log(weather);
+  if (!weather && favoriteCities) return <LoadingSpinner />;
+
   return (
     <>
       <header css={headerStyle}>
         <h1 css={headingStyle}>Your favorites</h1>
       </header>
-      {weather && <ul>{}</ul>}
-      {!weather && favoriteCities && <LoadingSpinner />}
-      {!favoriteCities && (
+      {favoriteCities.length ? (
+        <ul>
+          {/* {console.log(Array.isArray(weather))} */}
+          {weather?.map((city, index) => (
+            <li key={index}>
+              <h2>TEST</h2>
+              {/* <Link to={`/location/${city.name}`}>
+                <SmallWeatherInfoItem
+                  title={city.name}
+                  icon={city.weather[0].icon}
+                  avgTemp={city.main.temp}
+                />
+              </Link> */}
+            </li>
+          ))}
+        </ul>
+      ) : (
         <CenterContainer>
           <ImHeartBroken css={errorIconStyle} />
           <h2 css={errorMessageStyle}>You have no favorites</h2>
         </CenterContainer>
       )}
+
+      {/* {favoriteCities.length === 0 && (
+        <CenterContainer>
+          <ImHeartBroken css={errorIconStyle} />
+          <h2 css={errorMessageStyle}>You have no favorites</h2>
+        </CenterContainer>
+      )} */}
     </>
   );
 };
