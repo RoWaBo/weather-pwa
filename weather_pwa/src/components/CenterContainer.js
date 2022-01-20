ce03779e384cc69cf45bc5f21d6ae09d63a37d95
Robert <robertwattboolsen@gmail.com>
2022-01-20 10:02:47 +0100

    Fixed CenterContainer styling

--- weather_pwa/src/components/CenterContainer.js
+++ weather_pwa/src/components/CenterContainer.js
@@ -3,12 +3,17 @@ import { css } from "@emotion/react";
 
 const CenterContainer = ({ children }) => {
   const containerStyle = css`
-    width: 100vw;
-    height: 100vh;
+    /* width: 100vw;
+    height: 100%; */
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
     padding: 1rem;
   `;
   return <div css={containerStyle}>{children}</div>;
