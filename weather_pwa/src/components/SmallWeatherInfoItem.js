a2e7d3de8e54c4f7d291610a7f2de8a53d4b2087
Robert <robertwattboolsen@gmail.com>
2022-01-20 13:34:35 +0100

    Styled SmallWeatherInfoItem

--- weather_pwa/src/components/SmallWeatherInfoItem.js
+++ weather_pwa/src/components/SmallWeatherInfoItem.js
@@ -1,11 +1,29 @@
 import { formatTemp } from "../helperFunctions";
 import { css } from "@emotion/react";
 /** @jsxImportSource @emotion/react */
+import FavoriteToggleBtn from "../components/FavoriteToogleBtn";
 
-const SmallWeatherInfoItem = ({ title, icon, avgTemp }) => {
+const SmallWeatherInfoItem = ({ title, icon, avgTemp, cityName }) => {
+  const sectionStyle = css`
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid rgba(59, 60, 58, 0.15);
+    padding: 2rem 0;
+  `;
+  const tempStyle = css`
+    font-size: 16px;
+    font-weight: 500;
+    width: 2.2rem;
+    text-align: end;
+  `;
+  const titleStyle = css`
+    font-size: 18px;
+    margin-right: auto;
+  `;
+
   return (
-    <section>
-      <h2>{title}</h2>
+    <section css={sectionStyle}>
+      <h2 css={titleStyle}>{title}</h2>
       <div
         css={css`
           mask: url(${`./weatherIcons/${icon}.svg`}) no-repeat center;
@@ -15,7 +33,7 @@ const SmallWeatherInfoItem = ({ title, icon, avgTemp }) => {
           background: #3b3c3a;
         `}
       ></div>
-      <span>{formatTemp(avgTemp)}</span>
+      <span css={tempStyle}>{formatTemp(avgTemp)}</span>
     </section>
   );
 };
