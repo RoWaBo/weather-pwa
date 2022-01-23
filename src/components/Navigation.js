0f9845b67e6e7294d28cf8a4a6ffb029bb44a91f
Robert <u1501851@rts-365.dk>
2022-01-23 08:50:29 +0100

    Created neomorphism navigation link - styling not final

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { IoLocationSharp } from "react-icons/io5";
 import { IoMdHeart, IoMdSearch } from "react-icons/io";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
 const Navigation = () => {
+
+  const toggleLinkStyle = isActive => {
+    const passiveLink = {
+      boxShadow: '20px 20px 60px rgba(0, 0, 0, .4), -20px -20px 60px #fffcfd'
+    }
+    const activeLink = {
+      boxShadow: 'inset 20px 20px 60px rgba(0, 0, 0, .09), inset -20px -20px 60px #fffcfd'
+    }
+
+    return isActive ? activeLink : passiveLink
+  }
+
   const navStyle = css`
     position: fixed;
     bottom: 0;
     width: 100vw;
+    padding: .5rem 2rem;
     background: #fffcfd;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     border-radius: 20px 20px 0 0;
@@ -23,8 +36,12 @@ const Navigation = () => {
     font-size: 1.5rem;
   `;
   const linkStyle = css`
-    padding: 1rem 2rem;
+  /* padding: 1rem 2rem; */
+    padding: .5rem;
     display: flex;
+    border-radius: 50%;
+    background: #FFF;
+    transition: 0.25s;
 
     & > svg {
       color: #3b3c3a;
@@ -35,19 +52,19 @@ const Navigation = () => {
     <nav css={navStyle}>
       <ul css={listStyle}>
         <li css={itemStyle}>
-          <Link to="/favorites" css={linkStyle}>
+          <NavLink to="/favorites" css={linkStyle} style={({ isActive }) => toggleLinkStyle(isActive)}>
             <IoMdHeart />
-          </Link>
+          </NavLink>
         </li>
         <li css={itemStyle}>
-          <Link to="/" css={linkStyle}>
+          <NavLink to="/" css={linkStyle} style={({ isActive }) => toggleLinkStyle(isActive)}>
             <IoLocationSharp />
-          </Link>
+          </NavLink>
         </li>
         <li css={itemStyle}>
-          <Link to="/search" css={linkStyle}>
+          <NavLink to="/search" css={linkStyle} style={({ isActive }) => toggleLinkStyle(isActive)}>
             <IoMdSearch />
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
